d53925b1b9e786f53ada01903dfa9ac1084144f7
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-02-02 23:19:58 -0500

    Correccion Javascript 4 Ej 1

--- Javascript 4/Devolver propiedad de objeto anidado.js
+++ Javascript 4/Devolver propiedad de objeto anidado.js
@@ -2,13 +2,26 @@
 // Escribir una función que tome un objeto como argumento. En algunos casos, el objeto contiene otros objetos con algunas propiedades profundamente anidadas.
 // Devuelve el valor de la propiedad 'b' del objeto 'a' dentro del objeto original si existe, si no, devuelve undefined.
 
-console.log("********* ejercicio 1 *********");
-
 const searchingValueObj = (obj) => {
-  if (!obj.a) return undefined;
-  if (!obj.a?.b) return undefined;
+  if (obj.a && obj.a.b) {
+    return obj.a.b;
+  }
 
-  return obj.a.b;
+  let arr = Object.values(obj);
+
+  while (arr.length !== 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "object") {
+        arr.splice(i, 1);
+      } else if (arr[i]["a"] && arr[i]["a"]["b"]) {
+        return arr[i]["a"]["b"];
+      } else {
+        let newArr = Object.values(arr[i]);
+        arr.splice(i, 1);
+        arr = arr.concat(newArr);
+      }
+    }
+  }
 };
 
 console.log(searchingValueObj({ a: 1 })); // undefined
